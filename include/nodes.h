58b5816e676b97021c78bbeb6d948607685e0624
Amaryllis750 <danielchukwuemela751@gmail.com>
2026-05-11 03:52:55 +0100

    feat: started implementation of recursive descent

--- include/nodes.h
+++ include/nodes.h
@@ -4,51 +4,51 @@
 #include "lexer.h"
 
 typedef struct{
-    Token *word;
+    Token word;
 }Word;
 
 // Command = WORD WORD*
 typedef struct{
-    Word *program;
+    Word program;
     Word *args; // pointer to an array of Words
+    size_t arg_count;
 } Command;
 
 
-typedef struct {
-    Token *redir_op; // redirection operator, >, >> ...
-    Command *command;
-}Redirection;
-
-// RedirectedCommand = RedirectedCommand REDIRECTION WORD | Command
 typedef struct{
-    Command *command;
-    Redirection *redirs; // a linkedlist of Redirection
-    int redir_num;  // the number of redirections e.g. ps aux | grep "bash" | echo has 2 redirects
+    Token redirection;
+    Command command;
+} RedirectedArray;
+
+// RedirectedCommand = Command (REDIRECTION Command)*
+typedef struct{
+    Command command;
+    RedirectedArray *r_array; // array of redirected commands
+    size_t r_array_size;
 } RedirectedCommand;
 
 typedef struct{
-    Token *pipe;
-    RedirectedCommand *r_command;
-} Piped;
+    Token pipe;
+    RedirectedCommand r_command;
+} PipedArray;
 
-// PipedCommand=PipedCommand '|' RedirectedCommand | RedirectedCommand
-typedef struct {
-    RedirectedCommand *r_commands; // linked list of redirected commands
-    Piped *piped; 
-    int piped_num;
+// PipedCommand = RedirectedCommand ('|' RedirectedCommand)*
+typedef struct{
+    RedirectedCommand r_command;
+    PipedArray *p_array; // array of piped commands
+    size_t p_array_size;
 } PipedCommand;
 
-
-typedef struct {
-    Token *sequence; // ;
-    PipedCommand *p_command;
-} Sequenced;
-
-// SequencedCommand = SequencedCommand ';' PipedCommand | PipedCommand
 typedef struct{
-    PipedCommand *p_command;
-    Sequenced *sequenced;    // linked list of sequenced commands
-    int sequenced_num;
+    Token sequence;
+    PipedCommand p_command;
+} SequencedArray;
+
+// SequencedCommand = PipedCommand (';' PipedCommand)*
+typedef struct {
+    PipedCommand p_command;
+    SequencedArray *s_array; // array of sequenced commands
+    size_t s_array_size;
 } SequencedCommand;
 
 #endif
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -4,6 +4,9 @@
 
 #include "lexer.h"
 #include "parser.h"
+#include "nodes.h"
+
+Token current_token = {};
 
 void parse_input(char *input, Parser *parser, Lexer *lexer){
     while(lexer->pos < strlen(input)){
@@ -15,6 +18,67 @@ void parse_input(char *input, Parser *parser, Lexer *lexer){
     }
     parser->mode = PS1; // if the parser has left the loop, it has gathered all tokens
 }
+
+
+/**
+ * Each of the recursive functions will return a tree or subtree. 
+ * For the case of the sequence command, the entire tree will be returned since it is the highest node
+ * For the other nodes, they would each return subtrees
+ */
+
+SequencedCommand sequence_command(Lexer *lexer){
+    Token token = get_next_token(lexer);
+
+    // pass the token into the piped command
+
+}
+
+PipedCommand piped_command(Lexer *lexer){
+    // pass the token to the redirected command
+    RedirectedCommand r_command = redirected_command(lexer);
+    
+}
+
+RedirectedCommand redirected_command(Lexer *lexer){
+    // pass the token to the command
+    Command command = get_command(lexer);
+}
+
+Command get_command(Lexer *lexer){
+    // pass the token to the word
+    Command command;
+    if (current_token.value == NULL) current_token = get_next_token(lexer);
+
+    if(current_token.type == TOK_BARE_WORD){
+        Word word = get_word();
+        command.program = word;
+    }
+    else{
+        // this shouldn't return command but should trigger some kind of error;
+        return command;
+    }
+
+    current_token = get_next_token(lexer);
+
+    while(current_token.type == TOK_BARE_WORD || current_token.type == TOK_DOUBLE_QUOTED_WORD || current_token.type == TOK_SINGLE_QUOTED_WORD){
+        // if the args array is empty, initialize it and place the value. If the array is not empty, increase the size and append the value
+        
+    }
+    
+}
+
+Word get_word(){
+    Word word = {current_token};
+    // empty the current token
+    empty_current_token(&current_token);
+    return word;
+}
+
+void empty_current_token(Token *token){
+    token->position = NULL;
+    token->value = NULL;
+    token ->type = NULL;
+};
 
 
 void reset_parser(Parser *parser){
